9976e1c6b780e4d6fdbbc11db45c5d6c58ae35f2
Anirban Dey <anirbandey303@gmail.com>
2019-03-16 02:20:55 +0530

    Some profile modifications

--- profile.php
+++ profile.php
@@ -212,7 +212,7 @@
                 </div>              
               <div class="row">
                 <div class="col-md-7">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d3736489.7218514383!2d90.21589792292741!3d23.857125486636733!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1506502314230" width="100%" height="315" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3684.4978321134113!2d88.4893499766134!3d22.560476542486537!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a020b267a3cdc13%3A0xb3b21d652126f40!2sUniversity+of+Engineering+%26+Management%2C+New+Town!5e0!3m2!1sen!2sin!4v1552681750529" width="100%" height="315" frameborder="0" style="border:0" allowfullscreen></iframe>
                   </div>
 
                   <div class="col-md-5">
